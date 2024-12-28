6ec0d4ce3cc6918f13305a46f9095c771473b3c7
slpascoal <slpascoal01@gmail.com>
2024-12-28 15:47:41 -0300

    Finalizar pré-configuração da Aplicação

---
+++ src/components/Register.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="title">Register a new account</h1>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api' : {
+        target: "http://localhost",
+        changeOrigin: true,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      },
+    },
+  },
 })
